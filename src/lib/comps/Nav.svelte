bc70639e967c9533b39671db29eadf274ad93034
Django Merope <dmerope@gmail.com>
2025-01-25 15:53:17 +0900

    Mobile nav

--- src/lib/comps/Nav.svelte
+++ src/lib/comps/Nav.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
 	import { onClickOutside } from 'runed';
-	import { fly } from 'svelte/transition';
+	import { fade, fly } from 'svelte/transition';
 	let mobileNavOpen: boolean = $state(false);
-
+	let { hideBackground }: { hideBackground?: boolean } = $props();
 	let container = $state<HTMLElement>();
 	onClickOutside(
 		() => container,
@@ -14,11 +14,15 @@
 
 <section>
 	<nav
-		transition:fly={{ duration: 300, x: 0, y: -100 }}
-		class="fixed inset-x-0 top-0 z-10 bg-gray-800 px-4 py-3.5 transition-colors duration-300 ease-in-out lg:px-10"
+		class="fixed inset-x-0 top-0 z-10 px-4 py-3.5 transition-colors duration-300 ease-in-out lg:px-10"
+		class:bg-gray-900={!hideBackground}
 	>
 		<div class="flex items-center justify-between">
-			<a class="text-xl font-semibold text-white" href="/">
+			<a
+				class="text-xl font-semibold text-white transition-opacity duration-300 ease-in-out"
+				class:opacity-0={hideBackground}
+				href="/"
+			>
 				<img src="/logos/white_logomark.svg" alt="Belcoda Foundation logo" class="h-7 w-auto" />
 			</a>
 			<button onclick={() => (mobileNavOpen = !mobileNavOpen)} class="text-white lg:hidden">
@@ -62,49 +66,36 @@
 				class="relative flex h-full w-full flex-col overflow-y-auto border-r bg-white py-8"
 				bind:this={container}
 			>
-				<div class="mb-16 flex items-center pr-6">
-					<a class="ml-16 mr-auto text-xl font-semibold leading-none text-gray-800" href="#">
-						<img class="h-7" src="/logos/logo.svg" alt="" width="auto" />
+				<div class="mb-16 mt-12 flex justify-center pr-6">
+					<a class="text-xl font-semibold leading-none text-gray-800" href="/">
+						<img class="h-7" src="/logos/belcoda_dark.png" alt="" width="auto" />
 					</a>
 				</div>
 				<div>
 					<ul>
 						<li class="mb-1">
 							<a
-								class="block rounded py-5 pl-16 font-semibold text-blue-800 hover:bg-blue-50"
-								href="#">About</a
+								class="block rounded py-5 pl-16 font-semibold text-gray-800 hover:bg-gray-50"
+								href="/about">About</a
 							>
 						</li>
 						<li class="mb-1">
 							<a
-								class="block rounded py-5 pl-16 font-semibold text-blue-800 hover:bg-blue-50"
-								href="#">Company</a
+								class="block rounded py-5 pl-16 font-semibold text-gray-800 hover:bg-gray-50"
+								href="/software">Software</a
 							>
 						</li>
 						<li class="mb-1">
 							<a
-								class="block rounded py-5 pl-16 font-semibold text-blue-800 hover:bg-blue-50"
-								href="#">Services</a
-							>
-						</li>
-						<li class="mb-1">
-							<a
-								class="block rounded py-5 pl-16 font-semibold text-blue-800 hover:bg-blue-50"
-								href="#">Testimonials</a
+								class="block rounded py-5 pl-16 font-semibold text-gray-800 hover:bg-gray-50"
+								href="/contact">Contact</a
 							>
 						</li>
 					</ul>
 				</div>
 				<div class="mt-auto px-10">
-					<div class="pt-6">
-						<a class="mb-2 block py-4 text-center font-semibold text-blue-800" href="#">Sign in</a
-						><a
-							class="block border-2 border-blue-800 py-4 text-center font-semibold uppercase leading-normal text-blue-800 hover:bg-blue-50"
-							href="#">Sign Up</a
-						>
-					</div>
-					<p class="mb-4 mt-6 text-center text-sm text-blue-400">
-						<span>© 2021 All rights reserved © Wireframes Corporation 2021</span>
+					<p class="mb-4 mt-6 text-center text-xs text-gray-500">
+						<span>© {new Date().getFullYear()} Belcoda Foundation. All rights reserved. </span>
 					</p>
 				</div>
 			</nav>
